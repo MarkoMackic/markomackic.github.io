f17fdfd0b733adb73aa902b899987f05d9c72918
Marko Mackic <markomackic@gmail.com>
2022-11-01 21:22:50 +0100

    Some more restructuring, to help me while writing this series

---
+++ build_dsn_assets.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/MarkoMackic/dsn/ffd925fe155f237aae969718c84f3bb0c1c2ffcb/pre_receive.py > _includes/dsn_series/pre_receive-ffd925fe155f237aae969718c84f3bb0c1c2ffcb.py
